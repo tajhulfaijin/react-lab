795ca0c3191a1dcb9ffad38e90dac61886009727
Tajhul F <mrazoelcalm@gmail.com>
2020-03-16 13:53:50 +0700

    init push

---
+++ src/Kotak.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+
+export default function Kotak() {
+  return (
+    <Box color="text.primary" component="span" m={1}>
+      <Button>Tombol Kotak</Button>
+    </Box>
+  );
+}
